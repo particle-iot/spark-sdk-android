c9015d0fd075ffbbb656a5e7846aeff80afd1935
Jens Knutson <jens.knutson@gmail.com>
2015-08-25 23:25:44 -0500

    Autoformatter phun

--- example_app/src/main/java/io/particle/cloudsdk/example_app/LoginActivity.java
+++ example_app/src/main/java/io/particle/cloudsdk/example_app/LoginActivity.java
@@ -10,9 +10,9 @@ import android.widget.EditText;
 
 import java.io.IOException;
 
-import io.particle.android.sdk.cloud.SparkCloud;
-import io.particle.android.sdk.cloud.SparkCloudException;
-import io.particle.android.sdk.cloud.SparkDevice;
+import io.particle.android.sdk.cloud.ParticleCloud;
+import io.particle.android.sdk.cloud.ParticleCloudException;
+import io.particle.android.sdk.cloud.ParticleDevice;
 import io.particle.android.sdk.utils.Async;
 import io.particle.android.sdk.utils.Toaster;
 
@@ -30,15 +30,14 @@ public class LoginActivity extends AppCompatActivity {
                         final String email = ((EditText) findViewById(R.id.email)).getText().toString();
                         final String password = ((EditText) findViewById(R.id.password)).getText().toString();
 
-                        // Don't do:
+                        // Don't:
                         AsyncTask task = new AsyncTask() {
                             @Override
                             protected Object doInBackground(Object[] params) {
                                 try {
-                                    SparkCloud.get(LoginActivity.this).logIn(email, password);
-                                }
-                                catch (final SparkCloudException e)
-                                {
+                                    ParticleCloud.get(LoginActivity.this).logIn(email, password);
+
+                                } catch (final ParticleCloudException e) {
                                     Runnable mainThread = new Runnable() {
                                         @Override
                                         public void run() {
@@ -60,21 +59,21 @@ public class LoginActivity extends AppCompatActivity {
 
                         //-------
 
-                        // Do:
-                        Async.executeAsync(SparkCloud.get(v.getContext()), new Async.ApiWork<SparkCloud, Integer>() {
-                            private SparkDevice mDevice;
+                        // DO!:
+                        Async.executeAsync(ParticleCloud.get(v.getContext()), new Async.ApiWork<ParticleCloud, Integer>() {
+
+                            private ParticleDevice mDevice;
+
                             @Override
-                            public Integer callApi(SparkCloud sparkCloud) throws SparkCloudException, IOException {
-                                sparkCloud.logIn(email,password);
+                            public Integer callApi(ParticleCloud sparkCloud) throws ParticleCloudException, IOException {
+                                sparkCloud.logIn(email, password);
                                 sparkCloud.getDevices();
                                 mDevice = sparkCloud.getDevice("1f0034000747343232361234");
                                 Integer variable;
                                 try {
                                     variable = mDevice.getVariable("analogvalue");
-                                }
-                                catch (SparkDevice.VariableDoesNotExistException e)
-                                {
-                                    Toaster.s(LoginActivity.this,"Error reading variable");
+                                } catch (ParticleDevice.VariableDoesNotExistException e) {
+                                    Toaster.s(LoginActivity.this, "Error reading variable");
                                     variable = -1;
                                 }
                                 return variable;
@@ -83,13 +82,13 @@ public class LoginActivity extends AppCompatActivity {
 
                             @Override
                             public void onSuccess(Integer value) {
-                                Toaster.l(LoginActivity.this,"Logged in");
+                                Toaster.l(LoginActivity.this, "Logged in");
                                 Intent intent = ValueActivity.buildIntent(LoginActivity.this, value, mDevice.getID());
                                 startActivity(intent);
                             }
 
                             @Override
-                            public void onFailure(SparkCloudException e) {
+                            public void onFailure(ParticleCloudException e) {
                                 Toaster.l(LoginActivity.this, e.getBestMessage());
                                 e.printStackTrace();
                                 Log.d("info", e.getBestMessage());
@@ -102,6 +101,5 @@ public class LoginActivity extends AppCompatActivity {
 
         );
     }
-
 
 }
--- example_app/src/main/java/io/particle/cloudsdk/example_app/ValueActivity.java
+++ example_app/src/main/java/io/particle/cloudsdk/example_app/ValueActivity.java
@@ -9,9 +9,9 @@ import android.widget.TextView;
 
 import java.io.IOException;
 
-import io.particle.android.sdk.cloud.SparkCloud;
-import io.particle.android.sdk.cloud.SparkCloudException;
-import io.particle.android.sdk.cloud.SparkDevice;
+import io.particle.android.sdk.cloud.ParticleCloud;
+import io.particle.android.sdk.cloud.ParticleCloudException;
+import io.particle.android.sdk.cloud.ParticleDevice;
 import io.particle.android.sdk.utils.Async;
 import io.particle.android.sdk.utils.Toaster;
 
@@ -33,48 +33,43 @@ public class ValueActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //...
-                    // Do network work on background thread
-                    Async.executeAsync(SparkCloud.get(ValueActivity.this), new Async.ApiWork<SparkCloud, Integer>() {
-                        @Override
-                        public Integer callApi(SparkCloud sparkCloud) throws SparkCloudException, IOException {
-                            SparkDevice device = sparkCloud.getDevice(getIntent().getStringExtra(ARG_DEVICEID));
-                            Integer variable;
-                            try {
-                                variable = device.getVariable("analogvalue");
-                            }
-                            catch (SparkDevice.VariableDoesNotExistException e)
-                            {
-                                Toaster.l(ValueActivity.this, e.getMessage());
-                                variable = -1;
-                            }
-                            return variable;
+                // Do network work on background thread
+                Async.executeAsync(ParticleCloud.get(ValueActivity.this), new Async.ApiWork<ParticleCloud, Integer>() {
+                    @Override
+                    public Integer callApi(ParticleCloud ParticleCloud) throws ParticleCloudException, IOException {
+                        ParticleDevice device = ParticleCloud.getDevice(getIntent().getStringExtra(ARG_DEVICEID));
+                        Integer variable;
+                        try {
+                            variable = device.getVariable("analogvalue");
+                        } catch (ParticleDevice.VariableDoesNotExistException e) {
+                            Toaster.l(ValueActivity.this, e.getMessage());
+                            variable = -1;
                         }
+                        return variable;
+                    }
 
-                        @Override
-                        public void onSuccess(Integer i) // this goes on the main thread
-                        {
-                            tv.setText(i.toString());
-                        }
+                    @Override
+                    public void onSuccess(Integer i) // this goes on the main thread
+                    {
+                        tv.setText(i.toString());
+                    }
 
-                        @Override
-                        public void onFailure(SparkCloudException e) {
-                            e.printStackTrace();
-                        }
-                    });
+                    @Override
+                    public void onFailure(ParticleCloudException e) {
+                        e.printStackTrace();
+                    }
+                });
             }
         });
     }
 
-    public static Intent buildIntent(Context ctx, Integer value, String deviceid)
-    {
+    public static Intent buildIntent(Context ctx, Integer value, String deviceid) {
         Intent intent = new Intent(ctx, ValueActivity.class);
         intent.putExtra(ARG_VALUE, value);
         intent.putExtra(ARG_DEVICEID, deviceid);
 
         return intent;
     }
-
-
 
 
 }
