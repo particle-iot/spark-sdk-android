f5e3dde48a5c2e8012a3bb15b9ad9f4f7b8562ea
Jens Knutson <jens.knutson@gmail.com>
2016-02-04 04:08:33 -0600

    Use much, much smarter, more intentional executor config

--- cloudsdk/src/main/java/io/particle/android/sdk/cloud/SDKProvider.java
+++ cloudsdk/src/main/java/io/particle/android/sdk/cloud/SDKProvider.java
@@ -68,11 +68,31 @@ class SDKProvider {
         // FIXME: see if this TokenGetterDelegate setter issue can be resolved reasonably
         ParticleCloud cloud = new ParticleCloud(apiFactory.getApiUri(), cloudApi, identityApi,
                 SDKGlobals.getAppDataStorage(), LocalBroadcastManager.getInstance(ctx),
-                apiFactory.getGsonInstance(), Executors.newCachedThreadPool());
+                apiFactory.getGsonInstance(), buildExecutor());
         // FIXME: gross circular dependency
         tokenGetter.cloud = cloud;
 
         return cloud;
+    }
+
+
+    private static ExecutorService buildExecutor() {
+        // lifted from AsyncTask's executor config
+        int CPU_COUNT = Runtime.getRuntime().availableProcessors();
+        int CORE_POOL_SIZE = CPU_COUNT + 1;
+        int MAXIMUM_POOL_SIZE = CPU_COUNT * 2 + 1;
+        int KEEP_ALIVE = 1;
+        // FIXME: how big should this queue be?
+        BlockingQueue<Runnable> poolWorkQueue = new LinkedBlockingQueue<>(1024);
+        ThreadFactory threadFactory = new ThreadFactory() {
+            private final AtomicInteger mCount = new AtomicInteger(1);
+            public Thread newThread(Runnable r) {
+                return new Thread(r, "Particle Exec #" + mCount.getAndIncrement());
+            }
+        };
+
+        return new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE,
+                TimeUnit.SECONDS, poolWorkQueue, threadFactory);
     }
 
 
