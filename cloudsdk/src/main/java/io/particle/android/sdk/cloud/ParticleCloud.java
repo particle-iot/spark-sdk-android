a99189c6d1d63e19209f3221876d7895f9940511
Julius <julius.skripkauskas@gmail.com>
2017-02-15 12:43:39 +0200

    nullable, builder

--- cloudsdk/src/main/java/io/particle/android/sdk/cloud/ParticleCloud.java
+++ cloudsdk/src/main/java/io/particle/android/sdk/cloud/ParticleCloud.java
@@ -575,52 +575,47 @@ public class ParticleCloud {
         Set<String> functions = set(Funcy.filter(completeDevice.functions, Funcy.<String>notNull()));
         Map<String, VariableType> variables = transformVariables(completeDevice);
 
-        return new DeviceState(
-                completeDevice.deviceId,
-                completeDevice.name,
-                completeDevice.isConnected,
-                functions,
-                variables,
-                completeDevice.version,
-                ParticleDeviceType.fromInt(completeDevice.productId),
-                completeDevice.platformId,
-                completeDevice.productId,
-                completeDevice.cellular,
-                completeDevice.imei,
-                completeDevice.currentBuild,
-                completeDevice.defaultBuild,
-                completeDevice.ipAddress,
-                completeDevice.lastAppName,
-                completeDevice.status,
-                completeDevice.requiresUpdate,
-                completeDevice.lastHeard
-        );
+        return new DeviceState.DeviceStateBuilder(completeDevice.deviceId, functions, variables)
+                .name(completeDevice.name)
+                .cellular(completeDevice.cellular)
+                .connected(completeDevice.isConnected)
+                .version(completeDevice.version)
+                .deviceType(ParticleDeviceType.fromInt(completeDevice.productId))
+                .platformId(completeDevice.platformId)
+                .productId(completeDevice.productId)
+                .imei(completeDevice.imei)
+                .currentBuild(completeDevice.currentBuild)
+                .defaultBuild(completeDevice.defaultBuild)
+                .ipAddress(completeDevice.ipAddress)
+                .lastAppName(completeDevice.lastAppName)
+                .status(completeDevice.status)
+                .requiresUpdate(completeDevice.requiresUpdate)
+                .lastHeard(completeDevice.lastHeard)
+                .build();
     }
 
     // for offline devices
     private DeviceState fromSimpleDeviceModel(Models.SimpleDevice offlineDevice) {
         Set<String> functions = new HashSet<>();
         Map<String, VariableType> variables = new ArrayMap<>();
-        return new DeviceState(
-                offlineDevice.id,
-                offlineDevice.name,
-                offlineDevice.isConnected,
-                functions,
-                variables,
-                "",  // gross, but what else are we going to do?
-                ParticleDeviceType.fromInt(offlineDevice.productId),
-                offlineDevice.platformId,
-                offlineDevice.productId,
-                offlineDevice.cellular,
-                offlineDevice.imei,
-                offlineDevice.currentBuild,
-                offlineDevice.defaultBuild,
-                offlineDevice.ipAddress,
-                "",
-                offlineDevice.status,
-                false,
-                offlineDevice.lastHeard
-        );
+
+        return new DeviceState.DeviceStateBuilder(offlineDevice.id, functions, variables)
+                .name(offlineDevice.name)
+                .cellular(offlineDevice.cellular)
+                .connected(offlineDevice.isConnected)
+                .version("")
+                .deviceType(ParticleDeviceType.fromInt(offlineDevice.productId))
+                .platformId(offlineDevice.platformId)
+                .productId(offlineDevice.productId)
+                .imei(offlineDevice.imei)
+                .currentBuild(offlineDevice.currentBuild)
+                .defaultBuild(offlineDevice.defaultBuild)
+                .ipAddress(offlineDevice.ipAddress)
+                .lastAppName("")
+                .status(offlineDevice.status)
+                .requiresUpdate(false)
+                .lastHeard(offlineDevice.lastHeard)
+                .build();
     }
 
 
