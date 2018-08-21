30fdd3ad1e1aac4c6314351f205718d3e9dbad9f
Jens Knutson <jens.knutson@gmail.com>
2018-08-21 09:32:22 -0500

    Add simple method for determining if the logged-in user owns a device

--- cloudsdk/src/main/java/io/particle/android/sdk/cloud/ParticleCloud.java
+++ cloudsdk/src/main/java/io/particle/android/sdk/cloud/ParticleCloud.java
@@ -3,10 +3,12 @@ package io.particle.android.sdk.cloud;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.util.ArrayMap;
+import android.util.Log;
 
 import com.google.gson.Gson;
 
@@ -42,6 +44,7 @@ import io.particle.android.sdk.cloud.models.SignUpInfo;
 import io.particle.android.sdk.persistance.AppDataStorage;
 import io.particle.android.sdk.utils.Funcy;
 import io.particle.android.sdk.utils.Funcy.Func;
+import io.particle.android.sdk.utils.Funcy.Predicate;
 import io.particle.android.sdk.utils.Py.PySet;
 import io.particle.android.sdk.utils.TLog;
 import retrofit.RetrofitError;
@@ -348,6 +351,21 @@ public class ParticleCloud {
             throw new ParticleCloudException(error);
         }
     }
+
+    @WorkerThread
+    public boolean userOwnsDevice(@NonNull String deviceId) throws ParticleCloudException {
+        String idLower = deviceId.toLowerCase();
+        try {
+            List<SimpleDevice> devices = mainApi.getDevices();
+            SimpleDevice firstMatch = Funcy.findFirstMatch(devices,
+                    testTarget -> idLower.equals(testTarget.id.toLowerCase())
+            );
+            return firstMatch != null;
+        } catch (RetrofitError error) {
+            throw new ParticleCloudException(error);
+        }
+    }
+
 
     // FIXME: devise a less temporary way to expose this method
     // FIXME: stop the duplication that's happening here
